80ae18c0e707a2cb82175364746e6114f9166d00
Jennifer <jenndestes@gmail.com>
2021-02-07 15:08:59 -0600

    commit tree 5653fdd98c4694494ff57bfcbf263407f69396e4

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,16 +1,13 @@
 def my_hash
+  {"key0" => "value0","key1" => "value1", "key2" => "value2"}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
 end
 
 
-def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
+def shipping_manifest # set a variable called `the_manifest`, equal to a hash
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3} # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+  return the_manifest
 end
 
 def retrieval
@@ -21,14 +18,16 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  shipping_manifest["oil paintings"]
 end
 
 def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
-                  "oil paintings" => 3
+                  "oil paintings" => 3, 
+                  "muskets" => 2,
+                  "gun powder" => 4
                  }
   # add 2 muskets to the shipping_manifest hash below
 
